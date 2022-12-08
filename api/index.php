55ce96899cbc69b78b04e3ee4acaa8d34aeae8fc
the_dev <the_dev@calape.ph>
2022-12-08 13:07:02 +0800

    work from laptop

--- api/index.php
+++ api/index.php
@@ -3,8 +3,44 @@ require 'db.php';
 require 'fn.php';
 
 $cmd = filter_input(INPUT_SERVER, 'HTTP_X_CMD');
+$req = filter_input(INPUT_SERVER, 'REQUEST_METHOD');
+
+if (!$req) exit;
 
 if ($cmd == 'add_member') {
+    $d = file_get_contents('php://input');
+    $d = base64_decode($d);
+    $d = json_decode($d, true);   
+
+    $required_fields = ['fname','lname','mname','dob','pob','purok','brgy'];
+
+    if (count($d) !== count($required_fields)) {
+        echo "required fields: " . implode(", ", $required_fields);
+        http_response_code(400);
+        exit;
+    }
+
+    foreach($required_fields as $rf) {
+        if (!filter_var($d[$rf]) && !charlimit($rf)) {
+            echo "missing required field: $rf";
+            http_response_code(400);
+            exit;
+        }
+
+        if ($rf == 'dob' && !checkdatefmt($d[$rf])) {
+            echo "invalid date format: $rf";
+            http_response_code(400);
+            exit;
+        }
+    }
+
+    $db = new Database();
+    $res = $db->addMember(array_values($d));
+
+    var_dump($res);
+}
+
+if ($cmd == 'member_info') {
     $d = file_get_contents('php://input');
     $d = base64_decode($d);
     $d = json_decode($d, true);   
