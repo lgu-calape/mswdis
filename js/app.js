6bddd4b8d1aea9480fbd14c6a177bf8fda3e0e17
dev1 <dev@calape.ph>
2022-12-22 14:50:52 +0800

    work-from-pc

--- js/app.js
+++ js/app.js
@@ -1,4 +1,10 @@
 const API = 'https://api.calape.ph/swdis/';
+const LNA = 'N/A';
+const LREQUIRED = 'Update required';
+
+function get_age(d) {
+    return Math.floor((new Date() - new Date(d).getTime()) / 3.15576e+10);
+}
 
 function get_param(k) {
     return new URLSearchParams(window.location.search).get(k);
